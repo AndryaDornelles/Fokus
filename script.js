e0613f2c38791814b57d8d6c91426abff2056967
“AndryaDornelles” <andryacds@gmail.com>
2025-07-13 15:27:22 -0300

    feat: create timer variables using DOM

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const html = document.querySelector('html');
+
 const btnFocus = document.querySelector('.app__card-button--foco');
 const btnShort = document.querySelector('.app__card-button--curto');
 const btnLong = document.querySelector('.app__card-button--longo');
+const btnStart = document.querySelector('.app__card-primary-butto-icon');
+
+const timerElement = document.getElementById('timer');
+const imageElement = document.querySelector('.app__image');
+const titleElement = document.querySelector('.app__title');
+
+const timerFocus = 1500;
+const timerShortRest = 300;
+const timerLongRest = 900;
 
 btnFocus.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
