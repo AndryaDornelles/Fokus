d66b1d4a0f6632c4883c1f207e008e237d76d2bf
“AndryaDornelles” <andryacds@gmail.com>
2025-07-13 17:59:24 -0300

    feat: Implemented audio music

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const btnShort = document.querySelector('.app__card-button--curto');
 const btnLong = document.querySelector('.app__card-button--longo');
 const btnStart = document.querySelector('.app__card-primary-butto-icon');
 const arrayBtn = document.querySelectorAll('.app__card-button');
+const musicInput = document.getElementById('alternar-musica');
 
 const timerElement = document.getElementById('timer');
 const imageElement = document.querySelector('.app__image');
 const titleElement = document.querySelector('.app__title');
+const music = new Audio('/sons/luna-rise-part-one.mp3');
+music.loop = true;
 
 const timerFocus = 1500;
 const timerShortRest = 300;
@@ -16,35 +19,49 @@ const timerLongRest = 900;
 
 btnFocus.addEventListener('click', () => {
     changeContext('foco');
+    btnFocus.classList.add('active');
 });
 
 btnShort.addEventListener('click', () => {
     changeContext('descanso-curto');
+    btnShort.classList.add('active');
 });
 
 btnLong.addEventListener('click', () => {
     changeContext('descanso-longo');
+    btnLong.classList.add('active');
 });
 
+musicInput.addEventListener('change', () => {
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
+})
+
 function changeContext(value) {
+    arrayBtn.forEach(function(value){
+        value.classList.remove('active');
+    });
     html.setAttribute('data-contexto', value);
     imageElement.setAttribute('src', `/imagens/${value}.png`);
-    
+
     switch (value) {
         case 'foco':
             titleElement.innerHTML = `
                 Otimize sua produtividade,<br>
-                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
             break;
         case 'descanso-curto':
             titleElement.innerHTML = `
                 Que tal dar uma respirada?<br>
-                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
             break;
         case 'descanso-longo':
             titleElement.innerHTML = `
                 Hora de voltar à superfície.<br>
-                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
             break;            
         default:
             break;
